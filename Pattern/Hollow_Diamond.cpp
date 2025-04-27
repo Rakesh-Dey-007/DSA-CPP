7e703042733c5c8c235425527970a94620537695
Rakesh-dey-007 <deyrakesh.official.2004@gmail.com>
2025-04-27 18:22:08 +0530

    Add Hollow Diamond Pattern.

---
+++ Pattern/Hollow_Diamond.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+// Hollow Diamond Pattern --->
+
+int main()
+{
+
+    int n = 4;
+
+    // Upper half of the diamond
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        if (i > 1)
+        {
+            for (int j = 1; j <= 2 * i - 3; j++)
+            {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // Lower half of the diamond
+
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        if (i > 1)
+        {
+            for (int j = 1; j <= 2 * i - 3; j++)
+            {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
